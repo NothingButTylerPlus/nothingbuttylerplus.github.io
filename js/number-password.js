ddbe4e5611a9296eb012bb1b7862a950cea4b5c9
NothingButTyler <tylerbloxofficialyt@gmail.com>
2025-01-07 19:02:50 -0700

    Create number-password.js

---
+++ js/number-password.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('number-password'); 
+// Prevent non-numeric input 
+input.addEventListener('keydown', function(event) { 
+// Allow only numbers and control keys 
+if (!((event.key >= '0' && event.key <= '9') || event.key === 'Backspace' || event.key === 'Delete')) { 
+event.preventDefault(); 
+} });
